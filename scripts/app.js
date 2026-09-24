f1b392c3b6b813abd9f6eeb8e5b59cdb4c95b244
hanabirhanuaragaw@gmail.com <hanabirhanuaragaw@gmail.com>
2026-09-24 23:30:58 +0300

    initialized project directory and installed dependencies

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const db = require("./db");
+const app = express();
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server started listening to http://localhost:${port}`);
+});
