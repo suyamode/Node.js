16ad7a2968be42b115c3bc4ef4b3d9fb74e5de10
hanabirhanuaragaw@gmail.com <hanabirhanuaragaw@gmail.com>
2026-09-25 01:28:50 +0300

    added the product router and imported it on app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
+require("dotenv").config();
 const db = require("./db");
+const productsRouter = require("../routes/products");
 const app = express();
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server started listening to http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+app.use(express.json);
+app.use(cors());
+app.use("/api/products", productsRouter);
+app.listen(PORT, () => {
+  console.log(`Server started listening to http://localhost:${PORT}`);
 });
